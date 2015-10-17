2b569e8f0c67796e76b2e66edce0e5f0c645b5bf
Ian Conery <ianconery@gmail.com>
2015-10-16 20:07:33 -0700

    removed console.log in palindromPermutations and started oneAway

---
+++ oneAway.js
@@ -0,0 +1,6 @@
+//given two strings determine if they are one or zero edits away
+//example in: 'hell', 'hello' out: true
+
+var oneAway = function(str1, str2){
+  
+};
--- palindromePermutation.js
+++ palindromePermutation.js
@@ -21,4 +21,3 @@ var palindromePermutations = function(str){
 };
 
 
-console.log(palindromePermutations("tacot cat"))
