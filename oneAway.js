344586c1be50dda8b57a5fbaab64cbb14ee991b8
Ian Conery <ianconery@gmail.com>
2015-10-19 17:02:09 -0700

    initial oneAway commit not finished

--- oneAway.js
+++ oneAway.js
@@ -2,5 +2,25 @@
 //example in: 'hell', 'hello' out: true
 
 var oneAway = function(str1, str2){
-  
+  var str1 = str1.split('');
+  var str2 = str2.split('');
+  var editCount = 0;
+  if(Math.abs(str1.length - str2.length) > 1){
+    return false;
+  }
+  if(str1.length === str2.length){
+    for(var i = 0; i < str1.length; i++){
+      if(str1[i] !== str2[i]){
+        editCount ++;
+        console.log(editCount, i);
+        if(editCount > 1){
+          return false;
+        }
+      }
+    }
+    return editCount === 0 ? "Zero Edits Away" : "One Edit Away";
+  }
+
 };
+
+console.log(oneAway('hello', 'hella'))
