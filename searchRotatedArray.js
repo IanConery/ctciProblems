f0d13be49e659879767f0542c0fb0c8f8b4752ac
Ian Conery <ianconery@gmail.com>
2015-10-24 17:09:28 -0700

    added comment to search rotated array

--- searchRotatedArray.js
+++ searchRotatedArray.js
@@ -1,6 +1,8 @@
 //given a sorted array of integers that has been rotated an unknown number of times, write a method to find an element in the array, you may assume that the original array was intitialy sorted in increasing order
 //Example input: [10,11,12,13,1,2,3,4,5], 5 output: 8 (the index of the array)
 
+
+//This is not working correctly, will come back to it later
 var searchRotatedArray = function(array, number){
   var first = 0;
   var last = array.length - 1;
