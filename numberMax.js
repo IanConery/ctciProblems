cf8df355f394a15d0644ea7ce139b4b5d5d48164
Ian Conery <ianconery@gmail.com>
2015-10-27 14:41:55 -0700

    number max

---
+++ numberMax.js
@@ -0,0 +1,6 @@
+//Write a method that finds the maximum of two numbers. You should not use if-else or any other comparison operator.
+
+//bit wise operators is the answer, however it still relies on a comparison so I just used a ternary statement
+var numberMax = function(num1, num2){
+  return num1 > num2 ? num1 : num2;
+};
